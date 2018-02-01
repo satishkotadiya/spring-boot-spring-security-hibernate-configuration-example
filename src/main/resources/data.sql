420b41de36a798f829d347fec34a6c395b6aa619
Satish Kotadiya <satishrkotadiya@gmail.com>
2018-02-01 12:13:30 +0530

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- encoded password = password
+INSERT INTO user (id, first_name, last_name, email, password) VALUES (1, 'firstname', 'lastname', 'youremail@gmail.com', '$2a$10$RyY4bXtV3LKkDCutlUTYDOKd2AiJYZGp4Y7MPVdLzWzT1RX.JRZyG');
+
+INSERT INTO role (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO role (id, name) VALUES (2, 'ROLE_MANAGER');
+INSERT INTO role (id, name) VALUES (3, 'ROLE_USER');
+
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 2);
